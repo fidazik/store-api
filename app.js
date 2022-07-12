d8383cedb62dfa789c259cdba817e5700bb3fa16
fidazik <fidelis.azubuike@gmail.com>
2022-07-12 11:37:05 +0100

    Make db connection async

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
+
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
 
 const notFoundMiddleware = require('./middleware/notFoundMiddleware');
-const errorMiddleware = require('./middleware/error-handler');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 //middleware
 app.use(express.json());
 
 //routes
-app.get('/api/v1', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.send("<h2><a href='/api/v1/products'>Products Store</a></h2>");
 });
 
+app.use('/api/v1/products', productsRouter);
+
 app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3006;
 
@@ -25,7 +31,7 @@ const start = async () => {
         //connectDB
         await connectDB(process.env.MONGO_URI);
         app.listen(port, () => {
-            console.log(`Server listening on port ${port}`);
+            console.log(`Server is listening on port ${port}`);
         })
     } catch (error) {
         console.log(error);
